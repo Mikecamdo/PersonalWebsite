2c4b35bfd8b7cbe86c03ceb830562a63451bffac
Mikecamdo <mcdoherty@smu.edu>
2023-03-15 21:41:21 -0500

    Added "About Me" section
    Added information the the "About Me" section

--- website/src/About.jsx
+++ website/src/About.jsx
@@ -1,5 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 export const About = () => {
     return <>
-        About me
+        <Container fluid>
+            <Row className='display-6 text-center mt-5'>
+                <Col>
+                    About Me
+                </Col>
+            </Row>
+            <Row className='text-center'>
+                <Col>
+                Born and raised in the Jackson, Mississippi area, I've enjoyed working with technology my whole life.
+                Since coming to college, I've discovered a passion for creating innovative solutions to problems through software.
+                I'm currently interested in AI, Machine Learning, and Software Development, and I hope to utilize my skills to make a tangible impact on the problems that face our society today.
+                </Col>
+            </Row>
+        </Container>
     </>
 }
--- website/src/Home.jsx
+++ website/src/Home.jsx
@@ -11,7 +11,6 @@ export const Home = () => {
             <Row className='text-center mt-3'>
                 <Col className='fs-5'>
                     I'm a student studying computer science at Southern Methodist University in Dallas, Texas.
-                    I'm interested in AI, Machine Learning and Software Development and hope to one day use my skills to make the world a better place.
                 </Col>
             </Row>
         </Container>
