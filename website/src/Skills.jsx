bf7802448d0ba0a0b58d4a2cba2e987c77b3c367
Mikecamdo <mcdoherty@smu.edu>
2023-03-15 22:10:00 -0500

    Skills Section
    Added info to skills section

--- website/src/Skills.jsx
+++ website/src/Skills.jsx
@@ -1,5 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export const Skills = () => {
     return <>
-        My skills
+        <Container fluid>
+            <Row className='display-6 text-center mt-5'>
+                <Col>
+                    Skills
+                </Col>
+            </Row>
+            <Row className='text-center'>
+                <Col>
+                    Programming: C/C++, Python, JavaScript, Java, HTML/CSS, Arm Assembly, SQL, React, Node.js
+                    <br/>
+                    Tools: Git, Scrum, Agile, Docker
+                </Col>
+            </Row>
+        </Container>
     </>
 }
