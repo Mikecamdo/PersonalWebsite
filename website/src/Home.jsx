2813aafe97a8cda9d3cebc57c4ef3940a30c31e4
Mikecamdo <mcdoherty@smu.edu>
2023-05-22 23:58:05 -0500

    Added picture of myself

--- website/src/Home.jsx
+++ website/src/Home.jsx
@@ -1,12 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 export const Home = () => {
     return <>
         <Container fluid>
             <Row className='text-center mt-5'>
                 <Col className='display-2'>Hello there! I'm <span className='blue-text'>Michael Doherty.</span></Col>
+                <Col>
+                    <Image fluid src='Image.jpeg'></Image>
+                </Col>
             </Row>
             <Row className='text-center mt-3'>
                 <Col className='fs-5'>
