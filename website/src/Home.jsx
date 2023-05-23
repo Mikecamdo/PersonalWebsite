44c58ffc8abc97600ee9333181379b1447791676
Mikecamdo <mcdoherty@smu.edu>
2023-05-23 00:16:37 -0500

    Better image and sizing

--- website/src/Home.jsx
+++ website/src/Home.jsx
@@ -6,17 +6,25 @@ import Image from 'react-bootstrap/Image';
 export const Home = () => {
     return <>
         <Container fluid>
-            <Row className='text-center mt-5'>
-                <Col className='display-2'>Hello there! I'm <span className='blue-text'>Michael Doherty.</span></Col>
-                <Col>
-                    <Image fluid src='Image.jpeg'></Image>
+            <Row className='align-items-center text-center mt-5'>
+                <Col xs={8} xxl={6} className='display-2'>
+                    <Row>
+                        <Col>
+                            Hello there! <br />
+                            I'm <span className='blue-text'>Michael Doherty.</span>
+                        </Col>
+                    </Row>
+                    <Row className='text-center mt-3'>
+                        <Col className='fs-5'>
+                            I'm a student studying computer science at Southern Methodist University in Dallas, Texas.
+                        </Col>
+                    </Row>
+                </Col>
+                <Col xs={4} xxl={6}>
+                    <Image fluid src='ProfessionalHeadshot.png'></Image>
                 </Col>
             </Row>
-            <Row className='text-center mt-3'>
-                <Col className='fs-5'>
-                    I'm a student studying computer science at Southern Methodist University in Dallas, Texas.
-                </Col>
-            </Row>
+
         </Container>
     </>
 }
