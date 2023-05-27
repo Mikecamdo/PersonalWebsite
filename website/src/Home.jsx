b72adb2ea5509061bcc8d4bfc784345c3c33743d
Mikecamdo <mcdoherty@smu.edu>
2023-05-27 13:13:56 -0500

    Full screen greeting
    Make the home section (the greeting part) take up the whole screen upon initially entering the website. Also centered the content vertically

--- website/src/Home.jsx
+++ website/src/Home.jsx
@@ -4,23 +4,22 @@ import Col from 'react-bootstrap/Col';
 
 export const Home = () => {
     return <>
-        <Container fluid>
-            <Row className='align-items-center text-center mt-5'>
-                <Col className='display-2'>
-                    <Row>
-                        <Col className='header'>
+        <Container className='full-screen d-flex align-items-center'>
+            <Row className='align-items-center text-center mx-auto'>
+                <Col>
+                    <Row className='display-1'>
+                        <Col className='header greeting'>
                             Hello there! <br />
                             I'm <span className='header blue-text'>Michael Doherty.</span>
                         </Col>
                     </Row>
                     <Row className='text-center mt-3'>
-                        <Col className='fs-5'>
+                        <Col className='fs-4'>
                             I'm a student studying computer science at Southern Methodist University in Dallas, Texas.
                         </Col>
                     </Row>
                 </Col>
             </Row>
-
         </Container>
     </>
 }
