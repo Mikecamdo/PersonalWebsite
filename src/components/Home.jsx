2b349e92baea0ffd3da101bb0a31891eb65667b2
Mikecamdo <mcdoherty@smu.edu>
2023-06-06 21:02:37 -0500

    Fade in for Home Section
    Implemented the fade in for the home section of the website

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,35 @@
+import React, { useState, useEffect, useRef } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import '../css/particles.css';
+import '../css/fadeIn.css';
 
 export const Home = () => {
+    const textRef = useRef(0);
+
+    const [loadText1, setLoadText1] = useState(false);
+    const [loadText2, setLoadText2] = useState(false);
+    const [loadText3, setLoadText3] = useState(false);
+
+    const handleScroll = () => {
+        const position = textRef.current.offsetTop;
+        console.log('HEREEEEE')
+        console.log(position)
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        setLoadText1(true);
+        setLoadText2(true);
+        setLoadText3(true);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);  
+
     return <>
         <Container className='full-screen d-flex align-items-center'>
             <div id='particles-container'>
@@ -11,16 +37,16 @@ export const Home = () => {
                 <div class="particles2"></div>
             </div>
 
-            <Row className='align-items-center text-center mx-auto'>
+            <Row ref={textRef} className='align-items-center text-center mx-auto'>
                 <Col>
                     <Row className='display-1'>
                         <Col className='header greeting'>
-                            Hello there! <br />
-                            I'm <span className='header gradient-text'>Michael Doherty.</span>
+                            <div className={`${loadText1 ? 'notHidden' : 'hidden'}`}>Hello there!</div>
+                            <div className={`${loadText1 ? 'notHidden delay-1' : 'hidden delay-1'}`}>I'm <span className='header gradient-text'>Michael Doherty.</span></div>
                         </Col>
                     </Row>
                     <Row className='text-center mt-3'>
-                        <Col className='fs-4'>
+                        <Col className={`fs-4 ${loadText1 ? 'notHidden delay-2' : 'hidden delay-2'}`}>
                             I'm a senior studying computer science at Southern Methodist University in Dallas, Texas.
                         </Col>
                     </Row>
