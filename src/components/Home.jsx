3f1d757d77d46c85850ec826c932f388bd4fd4b9
Mikecamdo <mcdoherty@smu.edu>
2023-06-06 21:07:35 -0500

    Code cleanup
    Got rid of unnecessary code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -6,28 +6,10 @@ import '../css/particles.css';
 import '../css/fadeIn.css';
 
 export const Home = () => {
-    const textRef = useRef(0);
-
     const [loadText1, setLoadText1] = useState(false);
-    const [loadText2, setLoadText2] = useState(false);
-    const [loadText3, setLoadText3] = useState(false);
-
-    const handleScroll = () => {
-        const position = textRef.current.offsetTop;
-        console.log('HEREEEEE')
-        console.log(position)
-    }
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll, { passive: true });
-
         setLoadText1(true);
-        setLoadText2(true);
-        setLoadText3(true);
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
     }, []);  
 
     return <>
@@ -37,16 +19,16 @@ export const Home = () => {
                 <div class="particles2"></div>
             </div>
 
-            <Row ref={textRef} className='align-items-center text-center mx-auto'>
+            <Row className='align-items-center text-center mx-auto'>
                 <Col>
                     <Row className='display-1'>
                         <Col className='header greeting'>
                             <div className={`${loadText1 ? 'notHidden' : 'hidden'}`}>Hello there!</div>
-                            <div className={`${loadText1 ? 'notHidden delay-1' : 'hidden delay-1'}`}>I'm <span className='header gradient-text'>Michael Doherty.</span></div>
+                            <div className={`delay-1 ${loadText1 ? 'notHidden' : 'hidden'}`}>I'm <span className='header gradient-text'>Michael Doherty.</span></div>
                         </Col>
                     </Row>
                     <Row className='text-center mt-3'>
-                        <Col className={`fs-4 ${loadText1 ? 'notHidden delay-2' : 'hidden delay-2'}`}>
+                        <Col className={`fs-4 delay-2 ${loadText1 ? 'notHidden' : 'hidden'}`}>
                             I'm a senior studying computer science at Southern Methodist University in Dallas, Texas.
                         </Col>
                     </Row>
